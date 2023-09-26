687242b1453608b5ed1f65cd83dd6a63657398e7
Deepanshu mehta <40016257+deepanshumehtaa@users.noreply.github.com>
2023-09-26 17:39:08 +0530

    Update 17_GoRotines_3.go

--- GoLang-Basic/17_GoRotines_3.go
+++ GoLang-Basic/17_GoRotines_3.go
@@ -64,4 +64,74 @@ func main() {
 
 
 
+func getUserName(_id int) string {
+	time.Sleep(time.Millisecond * 100)
+	if _id == 1 {
+		return "deepanshumehtaa"
+	} else {
+		panic("No id found !")
+	}
+}
+
+func getUserData(userName string, response_channel chan any) {
+	time.Sleep(time.Millisecond * 300)
+	if userName == "deepanshumehtaa" {
+		// data := make(map[any]string)
+		date := map[any]any{
+			"id":      1,
+			"Name":    "John",
+			"Age":     "30",
+			"Country": "USA",
+		}
+		response_channel <- date // pushing value into the channel
+
+	} else {
+		panic("No id found !")
+	}
+}
+
+func getUserFrinedsCount(userName string, response_channel chan any) {
+	time.Sleep(time.Millisecond * 100)
+	if userName == "deepanshumehtaa" {
+		response_channel <- 100
+	} else {
+		panic("No id found !")
+	}
+}
+
+func main() {
+	start := time.Now()
+
+	_id := 1
+	var userName string = getUserName(_id)
+
+	response_channel := make(chan any)
+	go getUserData(userName, response_channel)
+	go getUserFrinedsCount(userName, response_channel)
+
+	// value, ok := <-response_channel
+	// if !ok {
+	// 	fmt.Println("Channel is closed")
+	// } else {
+	// 	fmt.Println("Received:", value)
+	// }
+
+	// raise, error, bcoz channel dont have any value at that time
+	// if panic raised of status code 2:
+	// goroutine is waiting to receive a value from a channel, but no value is sent on that channel, and the channel is closed.
+	for resp := range response_channel {
+		fmt.Println("The Response for the channel is: ", resp)
+	}
+	close(response_channel)
+
+	// fmt.Printf("User id %d and username %s having data %v and Friend Couts %d", _id, userName, data, friendCount)
+	fmt.Println("Program ends in ", time.Since(start))
+
+} // main ends
+
+
+// 3rd Now its time to wait for the other workers to complete the work and at the end join the work
+
+
+
 
